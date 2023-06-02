520045f4601fe40cdb006a3c4fe61774558a46ce
vlada_caban <vladaonly@gmail.com>
2023-06-02 18:34:52 -0400

    added user questions

---
+++ index.js
@@ -0,0 +1,50 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+//An array of questions using inquirer package
+const questions = [
+  {
+    type: "input",
+    name: "logo_text_input",
+    message: "Please enter up to three characters for your logo: ",
+  },
+  {
+    type: "input",
+    name: "logo_color_input",
+    message:
+      "Please enter text color (either color keyword or a hexadecimal number): ",
+  },
+  {
+    type: "list",
+    name: "shape_input",
+    message: "Please select shape for your logo: ",
+    choices: ["Triangle", "Circle", "Square"],
+  },
+  {
+    type: "input",
+    name: "shape_color_input",
+    message:
+      "Please enter shape color (either color keyword or a hexadecimal number): ",
+  },
+
+];
+
+//function to create and write into README
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("Generated logo.svg")
+  );
+}
+
+//function to initialize the app
+function init() {
+  console.log("Initializing...");
+
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+    // writeToFile("./generatedREADME/README.md", generateMarkdown(answers));
+  });
+}
+
+//function call to initialize app
+init();
